0bd7267b414ed4d4c9a57061afdd88071042d4fb
Anoop Chillayi <achillayi@opentrends.net>
2020-05-07 11:50:41 +0530

    Components and props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//        Welcome to React + React
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+       Welcome to React + React
         </p>
         <a
           className="App-link"
@@ -20,7 +43,9 @@ function App() {
         </a>
       </header>
     </div>
-  );
+
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,100 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//pure function 
+function add(a, b) {
+  a = 50;
+
+
+  return a + b;
+
+}
+
+function Welcome1(props) {
+  var c = add(10, 20);
+  return (
+    <div>Welcome to react {props.name}</div>
+  )
+}
+
+class Welcome extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pincode: '680724'
+    }
+
+  }
+  componentDidMount() {
+    console.log("in did mount")
+
+
+    setTimeout(() => {
+
+      this.setState({
+        pincode: '000000'
+      })
+
+    }, 10000)
+  }
+
+  componentDidUpdate() {
+    console.log("in did update")
+  }
+  render() {
+    return (
+      <>
+        <div >Name : {this.props.name}</div>
+        <div >address : {this.props.address}</div>
+        <div >Pincode {this.state.pincode}</div>
+      </>
+
+    )
+  }
+
+}
+
+class App extends React.Component {
+
+  state = {
+    name: 'vishnu'
+  }
+
+  componentDidMount(){
+   
+    setTimeout(() => {
+
+      this.setState({
+        name: 'React'
+      })
+
+    }, 5000)
+  }
+  render() {
+    return (
+      <>
+        {/* <Welcome1 name="anoop" address="chalakudy" /> */}
+        <Welcome name={this.state.name} address="chalakudy" />
+      </>
+    )
+  }
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//         <h1>Another Render div </h1>,
+//   document.getElementById('root1')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
